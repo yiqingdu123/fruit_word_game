75cafeea688e9408d68cd59848016e5eddcd9c61
ericbirb <ericbi384@gmail.com>
2023-01-20 23:26:14 -0500

    added back button to MP

---
+++ client/src/components/pages/MultiPlayerGame.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "@reach/router";
+
+const MultiPlayerGame = (props) => {
+  return (
+    <div>
+      <h1>Multi Player</h1>
+      <p>Here is the multiplayer game.</p>
+      <h1>
+        <Link to="/">Back </Link>
+      </h1>
+    </div>
+  );
+};
+
+export default MultiPlayerGame;
