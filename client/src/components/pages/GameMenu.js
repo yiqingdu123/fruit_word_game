7a81d1ff0b811ae71b5aac4fa9452d3dce91d6ee
ericbirb <ericbi384@gmail.com>
2023-01-21 18:03:36 -0500

    added log in requirement

--- client/src/components/pages/GameMenu.js
+++ client/src/components/pages/GameMenu.js
@@ -2,18 +2,39 @@ import React from "react";
 import { Link } from "@reach/router";
 
 const GameMenu = (props) => {
+  let loginModal = null;
+  if (!props.userId) {
+    loginModal = (
+      <div>
+        <h2>Please Login First!</h2>
+        <h1>
+          <Link to="/">Back </Link>
+        </h1>
+      </div>
+    );
+  }
+  let page = null;
+  if (props.userId) {
+    page = (
+      <div>
+        <h1>Game Menu</h1>
+        <h1>
+          <Link to="/singleplayer"> Single Player </Link>
+        </h1>
+        <h1>
+          <Link to="/multiplayer"> Multi Player </Link>
+        </h1>
+        <h1>
+          <Link to="/"> Back to Menu </Link>
+        </h1>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <h1>Game Menu</h1>
-      <h1>
-        <Link to="/singleplayer"> Single Player </Link>
-      </h1>
-      <h1>
-        <Link to="/multiplayer"> Multi Player </Link>
-      </h1>
-      <h1>
-        <Link to="/"> Back to Menu </Link>
-      </h1>
+      {loginModal}
+      {page}
     </div>
   );
 };
--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -4,16 +4,32 @@ import { Link } from "@reach/router";
 const Profile = (props) => {
   let loginModal = null;
   if (!props.userId) {
-    loginModal = <div> Please Login First! </div>;
+    loginModal = (
+      <div>
+        <h2>Please Login First!</h2>
+        <h1>
+          <Link to="/">Back </Link>
+        </h1>
+      </div>
+    );
+  }
+  let page = null;
+  if (props.userId) {
+    page = (
+      <div>
+        <h1>Profile</h1>
+        <p>Here is the profile.</p>
+        <h1>
+          <Link to="/">Back </Link>
+        </h1>
+      </div>
+    );
   }
 
   return (
     <div>
-      <h1>Profile</h1>
-      <p>Here is the profile.</p>
-      <h1>
-        <Link to="/">Back </Link>
-      </h1>
+      {loginModal}
+      {page}
     </div>
   );
 };
